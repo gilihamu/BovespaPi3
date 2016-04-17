0cbb90c1aeecad50fbdac93e91550f14ba6dec0d
gilihamu <gilihamu@gmail.com>
2016-04-17 02:59:50 -0300

    Implementação de anotação na classe

--- src/modelo/Empresa.java
+++ src/modelo/Empresa.java
@@ -5,10 +5,24 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
- * @author mariana
+ * @author Giliard
  */
-public class Empresa {
-    
+ @Entity
+ @Table(name="Empresa")
+public class Empresa implements Serializable{
+
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;   
 }
